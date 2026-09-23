14e26f0d62a343e3e68127d9a060d546ce9c4476
Ricardo Valencia <rvalencia@t2company.com.uy>
2026-09-22 21:59:09 -0300

    Initial Commit

---
+++ InstrumentsPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InstrumentsPractice
+//
+//  Created by Ricardo Valencia on 22/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InstrumentsPractice/InstrumentsPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstrumentsPracticeApp.swift
+//  InstrumentsPractice
+//
+//  Created by Ricardo Valencia on 22/9/26.
+//
+
+import SwiftUI
+
+@main
+struct InstrumentsPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
